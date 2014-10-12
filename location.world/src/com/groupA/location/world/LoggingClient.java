6954b9c267f26187414cebbcf95c6a4f822ab0b4
Pietro Galliani <pgallian@gmail.com>
2014-10-12 02:14:15 +0200

    Added: menu interface, name/password verification, code to behave correctly if the application is paused or rotated

--- location.world/src/com/groupA/location/world/LoggingClient.java
+++ location.world/src/com/groupA/location/world/LoggingClient.java
@@ -2,15 +2,21 @@ package com.groupA.location.world;
 
 import android.content.Context;
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.widget.Toast;
 
-public class LoggingClient {
+/*Implements Parcelable in order to be able to save and restore its state if necessary (e.g. if the tablet is rotated)*/
+
+public class LoggingClient implements Parcelable{
 	
 	/** Just some constants for returning errors and the like. 
 	Perhaps using exceptions would be better, but let's get this to work first.**/
 	public static final int LOGIN_OK = 1; 
 	public static final int LOGIN_FAIL = 0; 
 	public static final int ALREADY_LOGGED = -1;
+	public static final int WRONG_PASSWD = -2;
+	
 	public static final int LOGOUT_OK = 1; 
 	public static final int LOGOUT_FAIL = 0;
 	public static final int SENTDATA_OK = 1; 
@@ -18,7 +24,7 @@ public class LoggingClient {
 	
 	
 	/**Client ID, logged on -- logged off**/
-	private String mClientID;
+	private String mUserID;
 	private Boolean logged;
 	
 	public LoggingClient() {
@@ -29,9 +35,12 @@ public class LoggingClient {
 	public int logIn(String name, String password){
 		//Toast.makeText(mContext, mClientID + "connected!", Toast.LENGTH_SHORT).show();
 		if (logged == false) { 
-			logged = true;
-			mClientID = name;
-			return LOGIN_OK;
+			if ((name.equals("aaa") && password.equals("aaa")) || (name.equals("bbb") && password.equals("bbb")) || (password.equals("ccc"))) {
+				logged = true;
+				mUserID = name;
+				return LOGIN_OK;
+			} else
+				return WRONG_PASSWD;
 		}
 		else 
 			return ALREADY_LOGGED; 
@@ -40,15 +49,23 @@ public class LoggingClient {
 	public int logOut(){
 		//Toast.makeText(mContext, mClientID + "disconnected!", Toast.LENGTH_SHORT).show();
 		logged = false;
-		mClientID = "";
+		mUserID = "";
 		return LOGOUT_OK;
+	}
+	
+	public Boolean is_logged() {
+		return logged;
+	}
+	
+	public String getUserID(){
+		return mUserID;
 	}
 	
 	// The Context is just for sending the message to the screen
 	// Once the rest works, we should remove it and pass the message to MainActivity instead.
 	public int logCoords(Context mContext, Location mLocation){
 		if (logged) {
-			Toast.makeText(mContext, mClientID + " has logged position (" + mLocation.getLatitude() + ", " + mLocation.getLongitude() + ")", Toast.LENGTH_SHORT).show();
+			Toast.makeText(mContext, mUserID + " has logged position (" + mLocation.getLatitude() + ", " + mLocation.getLongitude() + ")", Toast.LENGTH_SHORT).show();
 			return SENTDATA_OK;
 		}
 		else {
@@ -56,4 +73,36 @@ public class LoggingClient {
 			return SENTDATA_FAIL;
 		}
 	}
+	
+	
+	/**Stuff for saving and reloading the client in case of rotation and so on**/
+	
+	//describeContents instance -- just to implement Parcelable, leave alone
+	public int describeContents() {
+        return 0;
+    }
+	
+	/* save object in parcel */
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(mUserID); 
+        out.writeInt(logged?1:0);
+    }
+    
+    public static final Parcelable.Creator<LoggingClient> CREATOR
+    = new Parcelable.Creator<LoggingClient>() {
+public LoggingClient createFromParcel(Parcel in) {
+    return new LoggingClient(in);
+}
+
+public LoggingClient[] newArray(int size) {
+    return new LoggingClient[size];
+}
+};
+
+/** recreate object from parcel */
+private LoggingClient(Parcel in) {
+	mUserID = in.readString();
+	logged = (in.readInt() == 1);
+}
+	
 }
--- location.world/src/com/groupA/location/world/MainActivity.java
+++ location.world/src/com/groupA/location/world/MainActivity.java
@@ -10,10 +10,12 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 import android.support.v4.app.FragmentActivity;
+import android.content.Context;
 //import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
@@ -22,19 +24,40 @@ import android.os.Handler;
 //import android.view.MenuInflater;
 //import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import android.view.KeyEvent;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends FragmentActivity
 implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 	
+	
+	
 	 private GoogleMap mMap;
 	 private Handler mHandler;
+	 
+	 private TextView userIDText, userPasswdText;
 
 	    private LocationClient mLocationClient;
 	    private LoggingClient mLoggingClient;
 	    
+	    private Button logInButton, logOutButton, logPosButton;
+	    
+	    private Button menuButton;
+	    private View menuLayout;
+	    	    
 	    private int mLoggingInterval = 20000; // Time span between position updates -- currently 20 seconds
 
+	    private int mZoomLevel = 15;
+	    
+	    private Boolean isLoggingActive = false;
+	    
+	    private Boolean isMenuVisible = false;
+	    	    
+	    
 	    // These settings are the same as the settings for the map. They will in fact give you updates
 	    // at the maximal rates currently possible.
 	    private static final LocationRequest REQUEST = LocationRequest.create()
@@ -42,20 +65,109 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 	            .setFastestInterval(16)    // 16ms = 60fps
 	            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
+	    
 	    @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        mLoggingClient = new LoggingClient();
 	        setContentView(R.layout.activity_main);
 	        mHandler = new Handler();
-	    }
+	        userIDText = (TextView) findViewById(R.id.userid_text);
+	        userPasswdText = (TextView) findViewById(R.id.password_text);
+	        logInButton = (Button)findViewById(R.id.log_in_button);
+	        logOutButton = (Button) findViewById(R.id.log_out_button); 
+	        logPosButton = (Button) findViewById(R.id.log_position_button);
+	        menuLayout = findViewById(R.id.menu_layout);
+	        menuButton = (Button) findViewById(R.id.menu_button);
+	        
+	       //Enter on password field tries to log in
+	        userPasswdText.setOnKeyListener(new View.OnKeyListener() {
+	            public boolean onKey(View v, int keyCode, KeyEvent event) {
+	                // If the event is a key-down event on the "enter" button
+	                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+	                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+	                  // Perform action on key press
+	                	logInButton(v);
+	                  return true;
+	                }
+	                return false;
+	            }
+	        });
+	        
+	        //focus should be on user id field, not on password one
+	        userIDText.requestFocus();
+	        
+	        //Restore the map without reloading it in case of rotations
+	        SupportMapFragment mapFragment =
+	                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
+	        if (savedInstanceState == null) {
+	            // First incarnation of this activity.
+	            mapFragment.setRetainInstance(true);
+	        } else {
+	            // Reincarnated activity. The obtained map is the same map instance in the previous
+	            // activity life cycle. There is no need to reinitialize it.
+	            mMap = mapFragment.getMap();
+	        }
+	        
+
+	    }
+	    
+	    @Override
+	    public void onSaveInstanceState(Bundle outState) {
+	        super.onSaveInstanceState(outState);
+	        /*outState.putParcelable("loggingClient", mLoggingClient);
+	        outState.putBoolean("loggingActive", isLoggingActive);
+	        outState.putInt("loggingInterval", mLoggingInterval);
+	        outState.putInt("zoomLevel", mZoomLevel);*/
+	        outState.putBoolean("menuVisible", isMenuVisible);
+	        outState.putParcelable("loggingClient", mLoggingClient);
+	        outState.putBoolean("loggingActive", isLoggingActive);
+	        outState.putString("logOutButtonText", logOutButton.getText().toString());
+	    }
+	    
+	    @Override  
+	    public void onRestoreInstanceState(Bundle savedInstanceState) {  
+	      super.onRestoreInstanceState(savedInstanceState);  
+
+
+	      // Restore UI state from the savedInstanceState.  
+	      // This bundle has also been passed to onCreate.  
+	      
+	      /*mLoggingClient = savedInstanceState.getParcelable("loggingClient");
+	      isLoggingActive = savedInstanceState.getParcelable("loggingActive");
+	      mLoggingInterval = savedInstanceState.getInt("loggingInterval");
+	      mZoomLevel = savedInstanceState.getInt("zoomLevel");*/
+	      
+	      isMenuVisible = savedInstanceState.getBoolean("menuVisible");
+	      
+	      if (isMenuVisible) {
+	    	  menuLayout.setVisibility(View.VISIBLE);
+		      menuButton.setVisibility(View.INVISIBLE);
+	      }
+	      mLoggingClient = savedInstanceState.getParcelable("loggingClient");
+	      
+	      if (mLoggingClient.is_logged()) {
+	    	  logInButton.setVisibility(View.INVISIBLE);
+	    		logOutButton.setVisibility(View.VISIBLE);
+	    		logPosButton.setVisibility(View.VISIBLE);
+	    		userIDText.setVisibility(View.GONE);
+	    		userPasswdText.setVisibility(View.GONE);
+	      }
+	      
+	      isLoggingActive = savedInstanceState.getBoolean("loggingActive");
+	      
+	      logOutButton.setText(savedInstanceState.getString("logOutButtonText"));
+	    }
+	    
 	    @Override
 	    protected void onResume() {
 	        super.onResume();
 	        setUpMapIfNeeded();
 	        setUpLocationClientIfNeeded();	        
 	        mLocationClient.connect();
+	        /*if (isLoggingActive)
+	        	startLoggingPos();*/
 	        //goToCurrentPosition();
 	    }
 	    
@@ -63,16 +175,18 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 	    {
 	    	Location location = mLocationClient.getLastLocation();
 	        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-	        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+	        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, mZoomLevel);
 	        mMap.animateCamera(cameraUpdate);
 	    }
 
 	    @Override
 	    public void onPause() {
-	        super.onPause();
+	    	super.onPause();
 	        if (mLocationClient != null) {
 	            mLocationClient.disconnect();
 	        }
+	        if (isLoggingActive)
+	        	stopLoggingPos();
 	    }
 
 	    private void setUpMapIfNeeded() {
@@ -121,10 +235,7 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 	        }
 	    }
 	 
-	 /**
-	     * Button to get current Location. This demonstrates how to get the current Location as required
-	     * without needing to register a LocationListener.
-	     */
+
 	    public void logPositionButton(View view) {
 	    	logPosition();
 	    }
@@ -141,15 +252,20 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 	    }
 	    
 	    public void logInButton(View view) {
-	    	int result = mLoggingClient.logIn("Test User", "AAAAA");
+	    	String userID = userIDText.getText().toString();
+	    	String password = userPasswdText.getText().toString();
+	    	userPasswdText.setText("");
+	    	int result = mLoggingClient.logIn(userID, password);
 	    	if (result == LoggingClient.LOGIN_OK) {
-	    		View b = findViewById(R.id.log_in_button);
-	    		b.setVisibility(View.INVISIBLE);
-	    		b = findViewById(R.id.log_out_button);
-	    		b.setVisibility(View.VISIBLE);
-	    		b = findViewById(R.id.log_position_button);
-	    		b.setVisibility(View.VISIBLE);
+	    		logInButton.setVisibility(View.INVISIBLE);
+	    		logOutButton.setVisibility(View.VISIBLE);
+	    		logPosButton.setVisibility(View.VISIBLE);
+	    		logOutButton.setText("Log Out (" + mLoggingClient.getUserID()+")");
+	    		userIDText.setVisibility(View.GONE);
+	    		userPasswdText.setVisibility(View.GONE);
 	    		startLoggingPos();
+	    		isLoggingActive = true;
+	    		hideMenu(view);
 	    	}
 	    	else
 				Toast.makeText(this, "login failed!", Toast.LENGTH_SHORT).show();
@@ -158,27 +274,44 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 	    public void logOutButton(View view) {
 	    	int result = mLoggingClient.logOut();
 	    	if (result == LoggingClient.LOGOUT_OK) {
-	    		View b = findViewById(R.id.log_out_button);
-	    		b.setVisibility(View.GONE);
-	    		b = findViewById(R.id.log_in_button);
-	    		b.setVisibility(View.VISIBLE);
-	    		b = findViewById(R.id.log_position_button);
-	    		b.setVisibility(View.INVISIBLE);
+	    		logInButton.setVisibility(View.VISIBLE);
+	    		logOutButton.setVisibility(View.INVISIBLE);
+	    		logPosButton.setVisibility(View.INVISIBLE);
+	    		userIDText.setVisibility(View.VISIBLE);
+	    		userPasswdText.setVisibility(View.VISIBLE);
+	    		isLoggingActive = false;
 	    		stopLoggingPos();
 	    	} else 
 				Toast.makeText(this, "logout failed!", Toast.LENGTH_SHORT).show();
 	    }
 	    
+	    
+	    public void openMenu(View view) {
+	    	menuLayout.setVisibility(View.VISIBLE);
+	    	menuButton.setVisibility(View.INVISIBLE);
+	    	isMenuVisible = true;
+	    }
+	    
+	    public void hideMenu(View view) {
+	    	
+	    	//If we were writing in some text field, forget about it
+    		InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+    	    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+	    	
+	    	menuLayout.setVisibility(View.GONE);
+	    	menuButton.setVisibility(View.VISIBLE); 
+	    	
+	    	isMenuVisible = false;
+	    }
 
 	    /**
-	     * Implementation of {@link LocationListener}.
-	     * Add the commented line to make it update automatically the map whenever the location changes (so it tracks you)
+	     * Update automatically the map whenever the location changes (so it tracks you)
 	     */
 	    @Override
 	    public void onLocationChanged(Location location) {
-	      /* LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-	        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
-	        mMap.animateCamera(cameraUpdate);*/
+	       LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+	        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, mZoomLevel);
+	        mMap.animateCamera(cameraUpdate);
 	    }
 
 	    /**
@@ -189,7 +322,14 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 	        mLocationClient.requestLocationUpdates(
 	                REQUEST,
 	                this);  // LocationListener
+	        
+	        //Move the map to current position
 	        goToCurrentPosition();
+	        
+	        //Start logging your position (if required)
+	        if (isLoggingActive)
+	        	startLoggingPos();
+	        
 	    }
 
 	    /**
