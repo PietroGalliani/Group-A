62b5e06540257eb4dfec850c83dfbfdc368488b4
Pietro Galliani <pgallian@gmail.com>
2014-10-15 15:06:49 +0200

    removed an unnecessary import from ErrorDialogFragment

--- location.world/src/com/groupA/location/world/ErrorDialogFragment.java
+++ location.world/src/com/groupA/location/world/ErrorDialogFragment.java
@@ -2,7 +2,6 @@ package com.groupA.location.world;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 /**
