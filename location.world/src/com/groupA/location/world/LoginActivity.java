aa4ec58301509dc6e9b2fc92543dbfc160fd8997
capx <canemxiao@gmail.com>
2014-12-01 13:34:56 +0000

    fix activity to make it look neat

--- location.world/src/com/groupA/location/world/LoginActivity.java
+++ location.world/src/com/groupA/location/world/LoginActivity.java
@@ -1,4 +1,5 @@
 package com.groupA.location.world;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -14,60 +15,66 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
-public class LoginActivity extends AsyncTask<String,Void,String>{
-public interface LoginListener {
-void loginSucceeded(int charSelected, String userID);
-void loginFailed(String userID, String message);
-}
-LoginListener mListener;
-public LoginActivity(LoginListener listener) {
-mListener = listener;
-}
-protected void onPreExecute(){
-}
-@Override
-protected String doInBackground(String... params) {
-// TODO Auto-generated method stub
-try{
-String username = (String)params[0];
-String pass = (String)params[1];
-int charSelected = Integer.parseInt(params[2]);
-String link="http://54.77.125.52/app/WebserverProto.php";
-String data = URLEncoder.encode("username", "UTF-8")
-+ "=" + URLEncoder.encode(username, "UTF-8");
-data += "&" + URLEncoder.encode("password", "UTF-8")
-+ "=" + URLEncoder.encode(pass, "UTF-8");
-URL url = new URL(link);
-URLConnection conn = url.openConnection();
-conn.setDoOutput(true);
-OutputStreamWriter wr = new OutputStreamWriter
-(conn.getOutputStream());
-wr.write( data );
-wr.flush();
-BufferedReader reader = new BufferedReader
-(new InputStreamReader(conn.getInputStream()));
-StringBuilder sb = new StringBuilder();
-String line = null;
-// Read Server Response
-while((line = reader.readLine()) != null)
-{
-sb.append(line);
-break;
-}
-if (sb.toString().equals(username)){
-Log.d("debug", sb.toString());
-Log.d("debug", username);
-mListener.loginSucceeded(charSelected, sb.toString());
-}
-else
-mListener.loginFailed(username, sb.toString());
-Log.d("debug", sb.toString());
-return sb.toString();
-}catch(Exception e){
-Log.e("exception","login", e);
-return e.getMessage();}
-}
-@Override
-protected void onPostExecute(String result){
-}
+
+public class LoginActivity extends AsyncTask<String, Void, String> {
+	public interface LoginListener {
+		void loginSucceeded(int charSelected, String userID);
+
+		void loginFailed(String userID, String message);
+	}
+
+	LoginListener mListener;
+
+	public LoginActivity(LoginListener listener) {
+		mListener = listener;
+	}
+
+	protected void onPreExecute() {
+	}
+
+	@Override
+	protected String doInBackground(String... params) {
+		// TODO Auto-generated method stub
+		try {
+			String username = (String) params[0];
+			String pass = (String) params[1];
+			int charSelected = Integer.parseInt(params[2]);
+			String link = "http://54.77.125.52/app/WebserverProto.php";
+			String data = URLEncoder.encode("username", "UTF-8") + "="
+					+ URLEncoder.encode(username, "UTF-8");
+			data += "&" + URLEncoder.encode("password", "UTF-8") + "="
+					+ URLEncoder.encode(pass, "UTF-8");
+			URL url = new URL(link);
+			URLConnection conn = url.openConnection();
+			conn.setDoOutput(true);
+			OutputStreamWriter wr = new OutputStreamWriter(
+					conn.getOutputStream());
+			wr.write(data);
+			wr.flush();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			// Read Server Response
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+				break;
+			}
+			if (sb.toString().equals(username)) {
+				Log.d("debug", sb.toString());
+				Log.d("debug", username);
+				mListener.loginSucceeded(charSelected, sb.toString());
+			} else
+				mListener.loginFailed(username, sb.toString());
+			Log.d("debug", sb.toString());
+			return sb.toString();
+		} catch (Exception e) {
+			Log.e("exception", "login", e);
+			return e.getMessage();
+		}
+	}
+
+	@Override
+	protected void onPostExecute(String result) {
+	}
 }
--- location.world/src/com/groupA/location/world/UpdateLocationActivity.java
+++ location.world/src/com/groupA/location/world/UpdateLocationActivity.java
@@ -1,4 +1,5 @@
 package com.groupA.location.world;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -9,48 +10,51 @@ import java.net.URLEncoder;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.TextView;
-public class UpdateLocationActivity extends AsyncTask<Double,Void,String>{
-//flag 0 means get and 1 means post.(By default it is get.)
-public UpdateLocationActivity() {
-}
-protected void onPreExecute(){
-}
-@Override
-protected String doInBackground(Double... arg0) {
-try{
-int missionID = 1;
-double longitude = (double)arg0[1];
-double latitude = (double)arg0[2];
-String link="http://54.77.125.52/app/uploadLocation.php";
-String data = URLEncoder.encode("longtitude", "UTF-8")
-+ "=" + longitude;
-data += "&" + URLEncoder.encode("latitude", "UTF-8")
-+ "=" + latitude;
-data += "&" + URLEncoder.encode("activity", "UTF-8")
-+ "=" + missionID;
-URL url = new URL(link);
-URLConnection conn = url.openConnection();
-conn.setDoOutput(true);
-OutputStreamWriter wr = new OutputStreamWriter
-(conn.getOutputStream());
-wr.write( data );
-wr.flush();
-BufferedReader reader = new BufferedReader
-(new InputStreamReader(conn.getInputStream()));
-StringBuilder sb = new StringBuilder();
-String line = null;
-// Read Server Response
-while((line = reader.readLine()) != null)
-{
-sb.append(line);
-break;
-}
-return sb.toString();
-}catch(Exception e){
-return new String("Exception: " + e.getMessage());
-}
-}
-@Override
-protected void onPostExecute(String result){
-}
+
+public class UpdateLocationActivity extends AsyncTask<Double, Void, String> {
+	// flag 0 means get and 1 means post.(By default it is get.)
+	public UpdateLocationActivity() {
+	}
+
+	protected void onPreExecute() {
+	}
+
+	@Override
+	protected String doInBackground(Double... arg0) {
+		try {
+			int missionID = 1;
+			double longitude = (double) arg0[1];
+			double latitude = (double) arg0[2];
+			String link = "http://54.77.125.52/app/uploadLocation.php";
+			String data = URLEncoder.encode("longtitude", "UTF-8") + "="
+					+URLEncoder.encode(String.valueOf(longitude), "UTF-8") ;
+			data += "&" + URLEncoder.encode("latitude", "UTF-8") + "="
+					+URLEncoder.encode(String.valueOf(latitude), "UTF-8") ;
+			data += "&" + URLEncoder.encode("activity", "UTF-8") + "="
+					+ URLEncoder.encode(String.valueOf(missionID), "UTF-8");
+			URL url = new URL(link);
+			URLConnection conn = url.openConnection();
+			conn.setDoOutput(true);
+			OutputStreamWriter wr = new OutputStreamWriter(
+					conn.getOutputStream());
+			wr.write(data);
+			wr.flush();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			// Read Server Response
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+				break;
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			return new String("Exception: " + e.getMessage());
+		}
+	}
+
+	@Override
+	protected void onPostExecute(String result) {
+	}
 }
